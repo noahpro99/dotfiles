231ccc25a127a505f0a3f604ba8187963b8fa007
noahpro <noahpro@gmail.com>
2025-05-26 05:04:30 -0400

    nvidia settings

--- root/etc/nixos/configuration.nix
+++ root/etc/nixos/configuration.nix
@@ -39,13 +39,14 @@ in
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = false;
-    powerManagement.finegrained = false;
+    powerManagement.finegrained = true;
     open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
   hardware.nvidia.prime = {
-    sync.enable = true;
+    reverseSync.enable = true;
+    allowExternalGpu = false;
     intelBusId = "PCI:0:2:0";
     nvidiaBusId = "PCI:1:0:0";
   };
