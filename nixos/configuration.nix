3a046d515d90413848cdbd9c4053fc018bd1ffe2
noahpro <noahpro@gmail.com>
2025-08-03 18:04:35 -0400

    udiskie

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -214,6 +214,7 @@ in
     networkmanagerapplet
     dotool # automate typing in wayland for nerd-dictation
     bibata-cursors # only for xcursors as fallback for hyprcursor on apps like gtk
+    udiskie # for mounting drives automatically and gui
   ];
 
   fonts.packages = with pkgs; [
@@ -289,11 +290,5 @@ in
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = "25.05"; # double check before changing this value https://nixos.org/nixos/options.html
 }
