fdd811fcf84ac2e81d40642afd4b988a680f60e0
noahpro <noahpro@gmail.com>
2026-01-27 07:25:24 -0500

    xdg-desktop portal

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     omenix.url = "github:noahpro99/omenix";
     tofi-emoji.url = "github:noahpro99/tofi-emoji";
     aether.url = "github:noahpro99/aether/nixos";
-    hyprland-preview-share-picker.url = "github:noahpro99/hyprland-preview-share-picker";
+    hyprland-preview-share-picker.url = "git+https://github.com/WhySoBad/hyprland-preview-share-picker?submodules=1";
   };
 
   outputs =
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -131,7 +131,7 @@
       pwvucontrol # gui for pipewire
       terminaltexteffects # tte for screensaver
       ghostty
-      inputs.hyprland-preview-share-picker.packages."${pkgs.stdenv.hostPlatform.system}".default
+      inputs.hyprland-preview-share-picker.packages.${pkgs.stdenv.hostPlatform.system}.default
     ]
     ++ [
       inputs.tofi-emoji.packages."${pkgs.stdenv.hostPlatform.system}".tofi-emoji
