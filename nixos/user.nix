73d5486f69d719f75cc18594ffb8272179a46c52
noahpro <noahpro@gmail.com>
2026-06-27 15:33:32 -0400

    Set chromium as default web/http handler
    So SAML-flow tools (e.g. openconnect for VT VPN) open the auth URL in
    chromium instead of vesktop.

--- nixos/user.nix
+++ nixos/user.nix
@@ -86,6 +86,14 @@ in
 
   programs.browserpass.enable = true;
 
+  xdg.mime.defaultApplications = {
+    "text/html" = "chromium-browser.desktop";
+    "x-scheme-handler/http" = "chromium-browser.desktop";
+    "x-scheme-handler/https" = "chromium-browser.desktop";
+    "x-scheme-handler/about" = "chromium-browser.desktop";
+    "x-scheme-handler/unknown" = "chromium-browser.desktop";
+  };
+
   services.tailscale.enable = true;
   networking.firewall = {
     trustedInterfaces = [ "tailscale0" ];
